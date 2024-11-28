68a92d3d9a66888c2d4154fc96fc626fc4901c02
Tomáš <tomas.pardygal@seznam.cz>
2024-11-28 01:15:14 +0100

    added template config

--- src/main/java/com/client/rcrm/integration/raynet/notification/email/EmailWithAttachmentNotification.java
+++ src/main/java/com/client/rcrm/integration/raynet/notification/email/EmailWithAttachmentNotification.java
@@ -3,6 +3,7 @@ package com.client.rcrm.integration.raynet.notification.email;
 import com.client.rcrm.integration.raynet.notification.NotificationDetails;
 import com.client.rcrm.integration.raynet.notification.NotificationStrategy;
 import com.client.rcrm.integration.raynet.notification.exception.EmailNotSendException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,43 +11,65 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
-import java.util.Objects;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Service("EmailWithAttachmentNotification")
 public class EmailWithAttachmentNotification implements NotificationStrategy {
 
     private final JavaMailSender javaMailSender;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final TemplateEngine templateEngine;
 
     @Value("${spring.mail.username}")
     private String sender;
 
-    public EmailWithAttachmentNotification(JavaMailSender javaMailSender) {
+    @Value("${raynet.username}")
+    private String recipient;
+
+    public EmailWithAttachmentNotification(JavaMailSender javaMailSender, TemplateEngine templateEngine) {
         this.javaMailSender = javaMailSender;
+        this.templateEngine = templateEngine;
     }
 
     @Override
-    public void sendNotification(NotificationDetails details) {
+    public void sendNotification(String emailDetails) {
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper mimeMessageHelper;
 
         try {
             mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
-            mimeMessageHelper.setFrom(sender);
-            mimeMessageHelper.setTo(details.getRecipient());
-            mimeMessageHelper.setText(details.getMsgBody());
-            mimeMessageHelper.setSubject(details.getSubject());
 
-            ClassPathResource resource = new ClassPathResource("data/upload-12077951414892126590-contact.csv");
-            details.addAttachment(Objects.requireNonNull(resource.getFilename()));
+            ClassPathResource classPathResource = new ClassPathResource("static/email_success.json");
 
-            for(String attachment : details.getAttachments()) {
-                mimeMessageHelper.addAttachment(Objects.requireNonNull(resource.getFilename()), resource);
+            try (InputStream inputStream = classPathResource.getInputStream()) {
+                NotificationDetails notificationDetails = objectMapper.readValue(inputStream, NotificationDetails.class);
+
+                // Set email details
+                mimeMessageHelper.setFrom(sender);
+                mimeMessageHelper.setTo(recipient);
+                mimeMessageHelper.setSubject(notificationDetails.getSubject());
+
+                // Set the content using Thymeleaf templates
+                Context context = new Context();
+                context.setVariable("username", recipient);
+                context.setVariable("content", notificationDetails.getMsgBody());
+
+                String processedString = templateEngine.process("mail_template", context);
+                mimeMessageHelper.setText(processedString, true);
+
+                // Send email
+                javaMailSender.send(mimeMessage);
             }
 
-            javaMailSender.send(mimeMessage);
         } catch (MessagingException e) {
-            throw new EmailNotSendException("Email wasn't send");
+            throw new EmailNotSendException("Email wasn't sent due to messaging error: " + e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading or parsing the email details: " + e.getMessage(), e);
         }
     }
 }
+
