73057b2717a63ed1b8ac8e133c13ca92bc296efb
Tomáš <tomas.pardygal@seznam.cz>
2024-12-04 23:40:43 +0100

    added file storage

---
+++ src/main/java/com/client/rcrm/integration/raynet/filestorage/config/StorageProperties.java
@@ -0,0 +1,16 @@
+package com.client.rcrm.integration.raynet.filestorage.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("storage")
+@Getter
+@Setter
+public class StorageProperties {
+
+    private String location = "upload-dir";
+
+}
